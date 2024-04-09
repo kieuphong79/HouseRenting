2ebb2368d077f3b768779da3a483881030878bc0
le tri <triledacminh@gmail.com>
2024-04-10 04:00:49 +0700

    commit

--- src/main/java/com/uet/model/House.java
+++ src/main/java/com/uet/model/House.java
@@ -2,7 +2,7 @@ package com.uet.model;
 
 
 public class House {
-    public static House sample = new House(2197, "Cho thuê nhà BT 80 m2 trên nền đất 215 m2, đường Phùng Khoang",1000000,  "Số điện thoại: 0913593202",
+    public static House sample = new House(2197, "Cho thuê nhà BT 80 m2 trên nền đất 215 m2, đường Phùng Khoang",1000000,  "Số điện thoại: 0913593202Số điện thoại: 0913593202Số điện thoại: 0913593202Số điện thoại: 0913593202Số điện thoại: 0913593202Số điện thoại: 0913593202Số điện thoại: 0913593202Số điện thoại: 0913593202Số điện thoại: 0913593202Số điện thoại: 0913593202Số điện thoại: 0913593202Số điện thoại: 0913593202Số điện thoại: 0913593202Số điện thoại: 0913593202",
      null, new Address("Đường Phùng Khoang", "Thành Phố Hà Nội", "Quận Nam Từ Liêm", "Phường Trung Văn"), 1, 1, 1, null, null, null, 10.0f, "https://cloud.muaban.net/images/thumb-detail/2024/03/14/473/fb9af1eb0ef04509ae33a9d02e83c2b9.jpg,https://cloud.muaban.net/images/thumb-detail/2024/03/14/473/29ca5e562f094b80bc91db99bb66b014.jpg,https://cloud.muaban.net/images/thumb-detail/2024/03/14/473/8cbcf60d174e42fba883c8e5c8d88bd8.jpg,https://cloud.muaban.net/images/thumb-detail/2024/03/14/473/48458f8d37b04df8a2eb36041bdd502f.jpg,https://cloud.muaban.net/images/thumb-detail/2024/03/14/473/87441328b3ee425d9f48dd1358237abc.jpg,https://cloud.muaban.net/images/thumb-detail/2024/03/14/472/efaba9991eac47f9974f0cc4588ab710.jpg,https://cloud.muaban.net/images/thumb-detail/2024/03/14/471/235efa8b1fb34b63879f31ff390a75af.jpg,https://cloud.muaban.net/images/thumb-detail/2024/03/14/472/c55f7e423ffc4064a103b3263ce087f6.jpg,https://cloud.muaban.net/images/thumb-detail/2024/03/14/472/0325f8f52f654fcbabc91317d1640801.jpg"
 , HouseType.HOUSE_LAND );
    /*id : "2198"
@@ -106,7 +106,12 @@ type : "HOUSE_LAND" //  APARTMENT, BEDSIT, HOUSE_LAND có 3 loại nhà
         this.imagesUrl = imagesUrl.split(",");
         this.houseType = houseType;
     }
-   
+    public String getTypeAsString() {
+        if (houseType == HouseType.APARTMENT) return "Chung cư";
+        else if (houseType == HouseType.HOUSE_LAND) return "Nhà nguyên căn";
+        else if (houseType == HouseType.BEDSIT) return "Phòng trọ";
+        throw new RuntimeException("Loi loai nha");
+    }
     
     
 
--- src/main/java/com/uet/view/HouseView.java
+++ src/main/java/com/uet/view/HouseView.java
@@ -3,6 +3,8 @@ package com.uet.view;
 
 import org.kordamp.ikonli.javafx.FontIcon;
 import org.kordamp.ikonli.material2.Material2AL;
+import org.kordamp.ikonli.material2.Material2MZ;
+import org.kordamp.ikonli.material2.Material2OutlinedAL;
 
 import com.uet.model.House;
 import com.uet.viewmodel.HouseViewModel;
@@ -18,6 +20,7 @@ import javafx.scene.control.Tooltip;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Material;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
 
@@ -40,8 +43,10 @@ public class HouseView extends ScrollPane{
         HBox firstHBox = new HBox();
         // firstHBox.setPadding(new Insets(10, 10, 10, 30));
         Text titleOfHouse = new Text(curHouse.getTitle());
+        titleOfHouse.setWrappingWidth(800);
         titleOfHouse.getStyleClass().addAll(Styles.TITLE_2);
         Text addressOfHouse = new Text(curHouse.getSpecAddress().toString());
+        addressOfHouse.setWrappingWidth(800);
         addressOfHouse.getStyleClass().addAll(Styles.TEXT_LIGHTER);
         HBox overallInformation = new HBox(creatInforVBox("Mức giá", curHouse.getPriceAsString()),
          creatInforVBox("Diện tích", String.valueOf(curHouse.getArea() + " m²")), 
@@ -61,12 +66,38 @@ public class HouseView extends ScrollPane{
         overallInformation.getChildren().add(actionContainer);
         overallInformation.setSpacing(30);
 
-        VBox imagePlusInformation = new VBox(imageShower, titleOfHouse, addressOfHouse, new Separator(), overallInformation, new Separator());
+        Text titleText = new Text("Thông tin mô tả");
+        titleText.getStyleClass().addAll(Styles.TITLE_3);
+        Text detail = new Text(curHouse.getDescirption());
+        detail.setWrappingWidth(400);
+        detail.getStyleClass().addAll(Styles.TEXT_NORMAL);
+
+        VBox detailContainer = new VBox(titleText, detail);
+        VBox.setMargin(titleText, new Insets(0, 0, 20, 0));
+
+        Text featureTitile = new Text("Đặc điểm");
+        VBox.setMargin(featureTitile, new Insets(20, 0, 0, 0));
+        featureTitile.getStyleClass().addAll(Styles.TITLE_3);
+        VBox left = new VBox(new Separator(), creatFeature(new FontIcon(Material2AL.GRID_ON), "Diện tích", String.valueOf(curHouse.getArea()) + " m²")
+        , new Separator(), creatFeature(new FontIcon(Material2MZ.SINGLE_BED), "Số phòng ngủ", String.valueOf(curHouse.getNumBedrooms()) + " phòng")
+        , new Separator(), creatFeature(new FontIcon(Material2OutlinedAL.BATHTUB), "Số toilet", String.valueOf(curHouse.getNumToilets()) + " phòng")
+        , new Separator());
+        VBox right = new VBox(new Separator(), creatFeature(new FontIcon(Material2AL.ATTACH_MONEY), "Mức giá", curHouse.getPriceAsString()) 
+        , new Separator(), creatFeature(new FontIcon(Material2AL.KITCHEN), "Số phòng bếp", String.valueOf(curHouse.getNumKitchens()) + " phòng")
+        , new Separator(), creatFeature(new FontIcon(Material2OutlinedAL.HOME), "Loại hình", curHouse.getTypeAsString()), new Separator());
+        HBox detailFeature = new HBox(left, right);
+        detailFeature.setSpacing(30);
+        
+        
+        VBox featureContainer = new VBox(featureTitile, detailFeature);
+        featureContainer.setSpacing(20);
+
+        VBox imagePlusInformation = new VBox(imageShower, titleOfHouse, addressOfHouse, new Separator(), overallInformation, new Separator(), detailContainer, featureContainer);
         
         imagePlusInformation.setSpacing(10);
         firstHBox.getChildren().addAll(imagePlusInformation);
         // imageShower.setStyle("-fx-background-color:red;");
-
+// lam tiep detail
         VBox container = new VBox();
         container.setPadding(new Insets(20, 10, 10, 50));
         container.getChildren().addAll(firstHBox);
@@ -79,6 +110,24 @@ public class HouseView extends ScrollPane{
         b.getStyleClass().addAll(Styles.TITLE_3);
         VBox res = new VBox(a, b);
         res.setSpacing(10);
+        res.setAlignment(Pos.CENTER_LEFT);
+        return res;
+    }
+    private HBox creatFeature(FontIcon icon, String title, String data) {
+        Text titleText = new Text(title);
+        titleText.getStyleClass().addAll( Styles.TEXT_BOLD);
+        Text dataText = new Text(data);
+        dataText.getStyleClass().addAll(Styles.TEXT_NORMAL);
+        icon.setIconSize(20);
+        HBox right = new HBox(dataText);
+        right.setAlignment(Pos.CENTER_RIGHT);
+        HBox.setHgrow(right, Priority.ALWAYS);
+        HBox res = new HBox(icon, titleText, right);
+        res.setPrefWidth(300);
+        res.setMaxWidth(300);
+        res.setMinWidth(300);
+        res.setSpacing(20);
+        HBox.setMargin(dataText, new Insets(0, 0, 0, 30));
         res.setAlignment(Pos.CENTER_LEFT);
         return res;
     }
